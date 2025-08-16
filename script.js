c868cd5b5ea8d2cf6c412d399718f4b9a90d99f7
flexult <touma.leandre.72@gmail.com>
2025-08-16 11:37:59 +0000

    création des variables boutons

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-alert("Test")
+const b1 = document.getElementById("b1");
+const b2 = document.getElementById("b2");
+const b3 = document.getElementById("b3");
+const b4 = document.getElementById("b4");
+const b5 = document.getElementById("b5");
+const b6 = document.getElementById("b6");
+const b7 = document.getElementById("b7");
+const b8 = document.getElementById("b8");
+const b9 = document.getElementById("b9");
+const b0 = document.getElementById("zero");
+
+const btn_add = document.getElementById("addition");
+const btn_sou = document.getElementById("soustraction");
+const btn_mul = document.getElementById("multiplication");
+const btn_div = document.getElementById("division");
+
+const btn_AC = document.getElementById("AC_btn");
+const btn_paran1 = document.getElementById("paran1");
+const btn_paran2 = document.getElementById("paran2");
+const btn_fac = document.getElementById("factorielle");
+const btn_virgule = document.getElementById("virtule");
+
+const btn_egal = document.getElementById("egal");
+
+let input = null;
